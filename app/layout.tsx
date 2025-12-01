d00249e6d35ef0ea406f67056860cb81a0a887db
orlifera <oferazzani125@gmail.com>
2025-12-01 17:39:56 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,29 +73,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
 
-        <Script id="segment-analytics" strategy="beforeInteractive">
+        <Script id="segment-analytics" strategy="afterInteractive">
           {`
-            !function(){
-              var analytics=window.analytics=window.analytics||[];
-              if(!analytics.initialize){
-                if(analytics.invoked){ console.error("Segment snippet included twice."); return; }
-                analytics.invoked=!0;
-                analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on"];
-                analytics.factory=function(method){ return function(){ var args=Array.prototype.slice.call(arguments); args.unshift(method); analytics.push(args); return analytics; }; };
-                for(var i=0;i<analytics.methods.length;i++){ var key=analytics.methods[i]; analytics[key]=analytics.factory(key); }
-                analytics.load=function(key){ 
-                  var script=document.createElement("script"); 
-                  script.type="text/javascript"; 
-                  script.async=!0; 
-                  script.src="https://cdn.segment.com/analytics.js/v1/"+key+"/analytics.min.js"; 
-                  var first=document.getElementsByTagName("script")[0]; 
-                  first.parentNode.insertBefore(script,first); 
-                };
-                analytics._writeKey="${process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY}";
-                analytics.SNIPPET_VERSION="5.2.1";
-                analytics.load("${process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY}");
-              }
-            }();
+            !function(){var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked = !0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");n.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey="${process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY}";;analytics.SNIPPET_VERSION="5.2.0";
+            analytics.load("${process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY}");
+            analytics.page();
+            }}();
           `}
         </Script>
 
