16b44f005e5b2b1d22bd176797fedfa8c0cdaaa7
orlifera <oferazzani125@gmail.com>
2025-12-01 16:51:50 +0100

    segment integration

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -107,9 +107,11 @@ export default function Banner({ src, src2, title, subtitle, icon, social, dwbtn
                     </div>
                     <p className="font-semibold text-center text-chart-3 text-lg mb-2">{subtitle}</p>
                     <div className="flex justify-center gap-4 mt-4">
+
                         <Link href="https://apps.apple.com/it/app/parkito-park-sharing/id6446240996">
                             <Image src="/applebtn.webp" alt="App Store" width={150} height={50} />
                         </Link>
+
                         <Link href="https://play.google.com/store/apps/details?id=it.autoindabox.mobile&hl=it">
                             <Image src="/googlebtn.webp" alt="Google Play" width={150} height={50} />
                         </Link>
