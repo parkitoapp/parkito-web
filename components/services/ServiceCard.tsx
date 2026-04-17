7c866c0bfa7c93ae0fb2c78d3756ccf99fab9ba5
orlifera <oferazzani125@gmail.com>
2026-04-17 11:51:40 +0200

    fix ServiceCard

--- components/services/ServiceCard.tsx
+++ components/services/ServiceCard.tsx
@@ -10,35 +10,33 @@ import { Button } from "@/components/ui/button";
 
 export default function ServiceCard({ service }: { service: ServiceType }) {
   const [hasError, setHasError] = useState(false);
-
   return (
 
-    <Link href={service.url}>
-      <Card className="shadow-md overflow-hidden bg-card hover:scale-[1.02] transition-transform duration-200 relative hover:shadow-lg rounded-3xl">
-        <Image
-          src={hasError ? service.fallbackImage : service.image}
-          alt={`Immagine per ${service.title}`}
-          width={400}
-          height={200}
-          loading="lazy"
-          className="rounded-t-3xl w-full h-40 object-cover mb-3"
-          onError={() => {
-            if (!hasError) {
-              setHasError(true);
-            }
-          }}
-        />
+    <Link href={service.url} className="h-full">
+      <Card className="shadow-md overflow-hidden bg-card hover:scale-[1.02] transition-transform duration-200 relative hover:shadow-lg rounded-3xl flex flex-col h-full">
+        <div className="relative w-full h-40 shrink-0">
+          <Image
+            src={hasError ? service.fallbackImage : service.image}
+            alt={`Immagine per ${service.title}`}
+            fill
+            loading="lazy"
+            className="rounded-t-3xl object-cover"
+            onError={() => {
+              if (!hasError) {
+                setHasError(true);
+              }
+            }}
+          />
+        </div>
 
-        <CardFooter className="flex flex-col gap-2 items-start px-6 py-4 w-full">
+        <CardFooter className="flex flex-col gap-2 items-start px-6 py-4 w-full flex-1">
           <CardTitle className="dark:text-chart-3 text-chart-4 text-xl text-left w-full">
             {service.title}
           </CardTitle>
 
-          {service.subtitle && (
-            <p className="text-sm text-muted-foreground text-left w-full">
-              {service.subtitle}
-            </p>
-          )}
+          <p className="text-sm text-muted-foreground text-left w-full flex-1">
+            {service.subtitle ?? ""}
+          </p>
 
           <Button
             variant="default"
@@ -47,7 +45,7 @@ export default function ServiceCard({ service }: { service: ServiceType }) {
             Scopri <ArrowRightIcon />
           </Button>
         </CardFooter>
-      </Card >
+      </Card>
     </Link>
   );
 }
